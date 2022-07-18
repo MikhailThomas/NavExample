7d23b16a02dfcfcfb616f6b7f7ee11ef50f1a33e
cassidymanuel01 <mikhailthomas29@gmail.com>
2022-07-18 12:06:59 +0200

    main

---
+++ Documents/javascript_workshop/lesson1/script/code.js
@@ -0,0 +1,39 @@
+// one line of comment
+
+/*
+multiple
+lines
+*/
+// variables
+/*
+
+Data types
+int, numbers: 4
+string:
+decimal,float,double: 4.5
+boolean, bool: true or false
+array
+object
+*/
+
+//different ways of creating variables
+/*
+let, var, const, use the variabel name
+*/
+number = 8;
+console.log(number);
+let firstname = 'Peter';
+let lastname = "Henk";
+console.log(firstname);
+console.log(lastname);
+var salary = 39988.98
+console.log(salary);
+const isMale = true;
+console.log(isMale);
+let subjects = ['HTML5', 'CSS3', 'Javascript', 'Jquery'];
+console.log(subjects);
+let details = {
+    name: 'Cassidy',
+    surname: 'Manuel'
+};
+console.log(details);
